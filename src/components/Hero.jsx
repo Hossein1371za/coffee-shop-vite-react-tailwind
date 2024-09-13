1ad76aa0b1b9b42d933c3e76e626248e09707626
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-09-13 22:12:42 +0330

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import BlackCoffee from "../assets/Black.png";
+import Navbar from "./Navbar";
 
 const Hero = () => {
   return (
     <main className="bg-hero bg-no-repeat bg-center bg-cover">
       <section className="min-h-[750px] w-full">
         <div className="container">
+            <Navbar />
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px]">
             <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
               <h1 className="text-7xl font-bold leading-tight ml-14">
@@ -33,7 +35,29 @@ const Hero = () => {
                 className="relative z-40 h-[400px] md:h-[700px] img-shadow"
               />
               <div className="h-[180px] w-[180px] absolute top-24 -right-16 border-[20px] border-primary rounded-full z-10 "></div>
-              <div></div>
+              <div className="absolute -top-10 left-[260px] z-[1]">
+                <h1 className="text-[100px] scale-150 font-bold text-darkGray/40 leading-none">
+                  Black Tumbler
+                </h1>
+              </div>
+            </div>
+            <div className=" text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
+              <h1 className="opacity-0 text-7xl font-bold leading-tight ml-14">
+                Black Tumbler
+              </h1>
+              <div className="relative">
+                <div className="relative z-10 space-y-4">
+                  <h1 className="text-2xl">Black Tumbler.</h1>
+                  <h1 className="text-sm opacity-55 leading-loose">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                  </h1>
+                </div>
+                <div className="absolute -top-6 -right-10 w-[250px] h-[190px] bg-darkGray/50">
+                  {" "}
+                </div>
+              </div>
             </div>
             <div></div>
           </div>
