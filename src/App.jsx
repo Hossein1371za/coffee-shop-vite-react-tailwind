c344485b4de3c025fd6baf1a70fce9a057a86835
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2024-09-13 21:36:06 +0330

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from "./components/Hero"
 
 const App = () => {
   return (
-    <div className='text-xl font-bold text-red-500'>App</div>
+    <div className='overflow-x-hidden'>
+      <Hero />
+    </div>
   )
 }
 
